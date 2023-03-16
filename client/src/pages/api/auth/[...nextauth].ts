fb13c0d13b6cd442697c8265178d24fbfb14dc98
web3ten0 <web3ten0@gmail.com>
2023-03-16 17:15:01 +0900

    Enabled GitHub authentication on client-side

---
+++ client/src/pages/api/auth/[...nextauth].ts
@@ -0,0 +1,17 @@
+import NextAuth from "next-auth";
+import GithubProvider from "next-auth/providers/github";
+import { PrismaAdapter } from "@next-auth/prisma-adapter";
+import { prisma } from "@mypj/database";
+
+export default NextAuth({
+  adapter: PrismaAdapter(prisma),
+  providers: [
+    GithubProvider({
+      clientId: process.env.GITHUB_ID ?? "",
+      clientSecret: process.env.GITHUB_SECRET ?? "",
+    }),
+  ],
+  session: {
+    strategy: "jwt",
+  },
+});
