396b86f4e19d184fef54d706438d6a0d3e4af44f
web3ten0 <web3ten0@gmail.com>
2023-03-17 00:00:14 +0900

    Adjust navbar design and add content

--- client/src/layouts/default.tsx
+++ client/src/layouts/default.tsx
@@ -1,5 +1,5 @@
-import Navbar from '../components/navbar'
 import Footer from '../components/footer'
+import { Navbar, Button, Link, Text, Container } from "@nextui-org/react";
 
 interface Props {
   children: React.ReactNode
@@ -8,8 +8,35 @@ interface Props {
 export default function Default({ children }: Props) {
   return (
     <>
-      <Navbar />
-      <main>{children}</main>
+      <Navbar isBordered={true} variant="sticky">
+        <Navbar.Brand>
+          <Text b color="black" hideIn="xs">
+            HOGE
+          </Text>
+        </Navbar.Brand>
+        <Navbar.Content activeColor="primary" hideIn="xs" variant="default">
+          <Navbar.Link href="#">Features</Navbar.Link>
+          <Navbar.Link isActive href="#">Customers</Navbar.Link>
+          <Navbar.Link href="#">Pricing</Navbar.Link>
+          <Navbar.Link href="#">Company</Navbar.Link>
+        </Navbar.Content>
+        <Navbar.Content>
+          <Navbar.Link color="inherit" href="#">
+            Login
+          </Navbar.Link>
+          <Navbar.Item>
+            <Button auto flat as={Link} color="primary" href="#">
+              Sign Up
+            </Button>
+          </Navbar.Item>
+        </Navbar.Content>
+      </Navbar>
+
+      <main>
+        <Container>
+          {children}
+        </Container>
+      </main>
       <Footer />
     </>
   )
