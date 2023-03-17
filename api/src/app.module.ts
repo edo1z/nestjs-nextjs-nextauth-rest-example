72af075540a60224f168e7ef9bc0968bbc3042c5
web3ten0 <web3ten0@gmail.com>
2023-03-17 17:37:44 +0900

    feat: Added Prisma module to NestJS

--- api/src/app.module.ts
+++ api/src/app.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { PostsModule } from './posts/posts.module';
+import { PrismaModule } from './prisma/prisma.module';
 
 @Module({
-  imports: [PostsModule],
+  imports: [PostsModule, PrismaModule],
   controllers: [AppController],
   providers: [AppService],
 })
